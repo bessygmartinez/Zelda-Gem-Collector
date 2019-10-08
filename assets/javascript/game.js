26b21f1c62731087710723ae0394bb0365eef982
Bessy Gee <nakiwarai13@gmail.com>
2019-10-08 18:15:02 -0400

    added game win/loss sounds

---
+++ assets/javascript/game.js
@@ -0,0 +1,157 @@
+$(document).ready(function() {
+
+	// Number Arrays
+
+	// random computer variable array
+	let rand = [];
+
+	for (let r = 19; r < 121; r++) {
+		rand.push(r);
+	}
+
+	// gem numbers array
+	let gems = [];
+
+	for (let g = 1; g < 13; g++) {
+
+		gems.push(g);
+	}
+
+    console.log(gems);
+    
+    //Gets link for audio
+    const youWinAudio = new Audio ("assets/sounds/BOTW_fanfare.wav");
+    const youLoseAudio = new Audio ("assets/sounds/LTTP_Link_Dying.wav");
+
+
+	// random variables selected by computer
+	let randomNumber; // number to match
+	let gemNumbers = []; // for array of random crystal values
+
+	let g1;
+	let g2;
+	let g3;
+	let g4;
+
+  let totalScore = 0; // user's score
+
+	let wins = 0;
+	let losses = 0;
+
+	// function to pick a random number
+	function pickRandomNumber(arr) {
+
+		let x = arr[Math.floor(Math.random() * arr.length)];
+		randomNumber = x;
+		$("#masterNumber").html(randomNumber);
+
+		console.log("random number: " + randomNumber);
+
+	}
+
+	// function to pick random numbers for gems
+
+	function pickRandomGems(arr) {
+
+		for (var y = 0; y < 4; y++){
+
+			var a = arr[Math.floor(Math.random() * arr.length)];
+
+			gemNumbers.push(a);
+		}
+    // console log the numbers that were picked for the gems
+		console.log("gem numbers: " + gemNumbers);
+
+	}
+
+	function gemValues(arr) {
+
+		// change value of each gem button according to array
+		for (i = 0; i < arr.length; i++) {
+
+		$(".gemImg" + (i+1)).attr("value", arr[i]);
+		console.log(this);
+		}
+		g1 = arr[0];
+		g2 = arr[1];
+		g3 = arr[2];
+		g4 = arr[3];
+    } 
+    
+    //Game Reset
+	function gameReset(x) {
+
+		crystalNumbers = []; // clears gem number values
+
+		pickRandomNumber(rand);
+
+		pickRandomGems(gems);
+
+		gemValues(gemNumbers);
+
+		totalScore = 0;
+        $("#yourTotal").html(totalScore);
+
+        alert(x);
+
+        youWinAudio.pause();
+        youLoseAudio.pause();
+	}
+
+	// Game start settings
+
+	pickRandomNumber(rand); // random number to match
+	pickRandomGems(gems); // array of random gem values
+	gemValues(gemNumbers);
+
+		// gem functions
+
+		$("#gem-1").on("click", function() {
+
+			totalScore += g1;
+			$("#yourTotal").html(totalScore);
+		});
+
+		$("#gem-2").on("click", function() {
+
+			totalScore += g2;
+			$("#yourTotal").html(totalScore);
+		});
+
+		$("#gem-3").on("click", function() {
+
+			totalScore += g3;
+			$("#yourTotal").html(totalScore);
+		});
+
+		$("#gem-4").on("click", function() {
+
+			totalScore += g4;
+			$("#yourTotal").html(totalScore);
+		});
+
+	$(".gemImg").on("click", function() {
+		// this is what happens if the user wins
+		if (totalScore == randomNumber) {
+
+			wins++;
+			console.log(totalScore);
+			$("#yourTotal").html(totalScore);
+            $("#playerWins").html(wins);
+            youWinAudio.play();
+
+			setTimeout(function() {gameReset("The Master Sword is yours!")}, 200);
+		}
+
+		else if (totalScore > randomNumber){
+
+			losses++;
+			$("#yourTotal").html(totalScore);
+            $("#playerLosses").html(losses);
+            youLoseAudio.play();
+
+			setTimeout(function() {gameReset("No Master Sword for you!")}, 200);
+		}
+	});
+
+});
